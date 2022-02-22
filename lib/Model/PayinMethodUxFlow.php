7da14fe913e04800a59f9b1883af574815f2a9d1
CodeBuild <>
2022-02-22 10:22:21 +0000

    BTA-7189 Add `transfer_reason` field (#283)
    [BTA-7189](https://btcafrica.atlassian.net/browse/BTA-7189): Add `transfer_reason` field
    
    Changes
    -------
    
    - add the `transfer_reason` string enum field to: `XOF::Bank`, `ZAR::Bank`, `KES::Bank` and `KES::Mobile`;
    - remove as being a required field the `transfer_reason_code` field. This will become a legacy field and then get removed soon, once our existing customers have switched to the new `transfer_reason` field. Validation to ensure the presence of one or the other have been added as part of the API changes;
    - bumping version as a minor update to `1.17.0`;
    
    API changes: https://github.com/bitpesa/bitpesa-api/pull/1714/
    
    Risk and impact assessment
    --------------------------
    
    The following PR contains (please tick anything that applies):
    
    * [ ] Version upgrades to the code generator
    * [ ] Adding new models to the specification
    * [ ] Contains a major change (see below)
    * [x] Changing existing models in the specification: `PayoutMethod`
    * [ ] Required additional steps required during release: [please specify]
    * [ ] Deemed high risk or impact for other reasons: [please specify]
    
    Please note that any major change in the existing models will break existing SDKs and should be avoided. Major changes include:
    
    * [ ] Any deletion of existing models, including auto-generated models
    * [ ] Any renaming of existing models, including auto-generated models
    * [ ] Any moving of existing models to a different place
    * [ ] Any removal of existing attributes
    * [ ] Any rename of existing attributes
    
    Any major change in the above requires an architectural approval

--- lib/Model/PayinMethodUxFlow.php
+++ lib/Model/PayinMethodUxFlow.php
@@ -34,7 +34,7 @@ use \TransferZero\ObjectSerializer;
  * PayinMethodUxFlow Class Doc Comment
  *
  * @category Class
- * @description Describes the collection experience that will be presented to the sender. Can be one of the following:  * &#x60;ussd_popup&#x60;: Payment prompt is pushed to the sender&#39;s phone. They can approve or decline. Supported for GHS::Mobile, UGX::Mobile * &#x60;bank_transfer&#x60;: For bank collections. Sender should send funds to the bank account details specified in out_details. Supported for EUR::Bank, GBP::Bank
+ * @description Describes the collection experience that will be presented to the sender. Can be one of the following:  * &#x60;ussd_popup&#x60;: Payment prompt is pushed to the sender&#39;s phone. They can approve or decline. Supported for GHS::Mobile, UGX::Mobile * &#x60;bank_transfer&#x60;: For bank collections. Sender should send funds to the bank account details specified in out_details. Supported for EUR::Bank, GBP::Bank * &#x60;http_redirect&#x60;: The sender will be redirected to a website where they can fill in their details to initiate the collection.
  * @package  TransferZero
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
@@ -46,6 +46,9 @@ class PayinMethodUxFlow
      */
     const USSD_POPUP = 'ussd_popup';
     const BANK_TRANSFER = 'bank_transfer';
+    const HTTP_REDIRECT = 'http_redirect';
+    const BLOCKCHAIN = 'blockchain';
+    const UNKNOWN = 'unknown';
     
     /**
      * Gets allowable values of the enum
@@ -56,6 +59,9 @@ class PayinMethodUxFlow
         return [
             self::USSD_POPUP,
             self::BANK_TRANSFER,
+            self::HTTP_REDIRECT,
+            self::BLOCKCHAIN,
+            self::UNKNOWN,
         ];
     }
 }
